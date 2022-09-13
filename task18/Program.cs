ad4e09ca3ef15c0496942a21f19ca572304e3add
Renata19 <renatalsa@mail.ru>
2022-09-13 18:58:39 +0700

    18

---
+++ task18/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Число от 1 до 4");
+int count = Convert.ToInt32(Console.ReadLine());
+
+
+
+if(count == 1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+}
+else if(count == 2)
+{
+    Console.WriteLine("x < 0 && y > 0 ");
+}
+else if(count == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if(count == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+else 
+{
+        Console.WriteLine("Такой четверти не существует");
+}
