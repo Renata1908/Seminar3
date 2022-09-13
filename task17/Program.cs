ab73a74773fe7b90ac1c948da6c3f0bb03184c3c
Renata19 <renatalsa@mail.ru>
2022-09-13 16:35:16 +0700

    17

---
+++ task17/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine("Введите координату Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату У");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0 )
+{
+    Console.WriteLine("Первая четверть");
+}
+else if(x < 0 && y > 0 )
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if(x < 0 && y < 0 )
+{
+    Console.WriteLine("Третья четверть");
+}
+else if(x > 0 && y < 0 )
+{
+    Console.WriteLine("Четвертая четверть");
+}
+
+
+
