ab779206cc50795078a0d08e05ed4f6de6504257
Renata19 <renatalsa@mail.ru>
2022-09-14 19:54:54 +0700

    22

---
+++ task22/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число N > 0:");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+Console.WriteLine();
+
+
+if(n > 0)
+{
+for (int i = 1; i <= n; i++)
+{
+    x = i * i;
+    Console.WriteLine(x);
+}
+}
+else
+Console.WriteLine("Введено некорректное число");
